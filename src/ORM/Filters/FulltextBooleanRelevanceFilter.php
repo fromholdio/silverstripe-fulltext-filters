b0aa5a5f2147101c77f198440f504e17a9fa6b89
Luke Fromhold <luke@fromhold.io>
2023-06-22 01:49:30 +1000

    Apply improved relevance filter manipulations

--- src/ORM/Filters/FulltextBooleanRelevanceFilter.php
+++ src/ORM/Filters/FulltextBooleanRelevanceFilter.php
@@ -2,6 +2,7 @@
 
 namespace Fromholdio\FulltextFilters\ORM\Filters;
 
+use SilverStripe\Core\Convert;
 use SilverStripe\ORM\DataQuery;
 
 class FulltextBooleanRelevanceFilter extends FulltextBooleanFilter
@@ -14,21 +15,67 @@ class FulltextBooleanRelevanceFilter extends FulltextBooleanFilter
         $this->setWeight($weight);
     }
 
+    /**
+     * fix string quoting for value
+     */
     protected function applyOne(DataQuery $query)
     {
         $value = $this->getValue();
-        if (!empty($value))
-        {
-            $query = parent::applyOne($query);
+        if ($value && strlen($value) > 0) {
+            // encode and quote
+            $value = Convert::raw2sql($value, true);
+            // copied from parent class, but use encoded value for where statement
+            $this->model = $query->applyRelation($this->relation);
+            $predicate = sprintf("MATCH (%s) AGAINST (? IN BOOLEAN MODE)", $this->getDbName());
+            $query->where([$predicate => $value]);
             // add select statement
             $alias = $this->getRelevanceAlias();
             $score = $this->getScoreName();
-            $select = sprintf("{$score} := MATCH (%s) AGAINST ('{$value}' IN BOOLEAN MODE)", $this->getDbName());
+            // escape % with %% for sprintf()
+            $value = str_replace('%', '%%', $value);
+            $select = sprintf("{$score} := MATCH (%s) AGAINST ({$value} IN BOOLEAN MODE)", $this->getDbName());
             $weight = $this->getWeight();
-            $select .= ' * ' . $weight;
+            $select = $select . ' * ' . $weight;
             $query->selectField($select, $alias);
         }
         return $query;
+    }
+
+    protected function excludeOne(DataQuery $query)
+    {
+        $this->model = $query->applyRelation($this->relation);
+        $predicate = sprintf("NOT MATCH (%s) AGAINST (? IN BOOLEAN MODE)", $this->getDbName());
+        return $query->where([$predicate => Convert::raw2sql($this->getValue(), true)]);
+    }
+
+    /**
+     * Override to skip regex processors to fix issues with special characters.
+     * Add + to all words to make search always use AND.
+     *
+     * {@inheritDoc}
+     * @see \Fromholdio\FulltextFilters\ORM\Filters\FulltextBooleanFilter::convertValue()
+     */
+    protected function convertValue($value)
+    {
+        // make sure search string is not empty
+        if (!is_string($value) || !strlen(trim($value))) {
+            return null;
+        }
+        $value = trim($value);
+
+        // remove everything except normal words, spaces and %
+        $value = preg_replace('/[^a-zA-Z0-9 %]/', ' ', $value);
+
+        // also remove % if not after number
+        $value = preg_replace('/([^0-9])%/', '$1', $value);
+
+        // remove multiple whitespace characters
+        $value = preg_replace('/[\s]{2,}/', ' ', trim($value));
+
+        // add + to all words to use AND for all words and numbers
+//        $value = preg_replace('/([a-zA-Z0-9]+)/', '+$1', $value);
+
+        return $value;
     }
 
     public function setWeight(int $weight)
